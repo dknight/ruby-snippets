2d16d56b44bc7bc9a82235a4867ff1a9f4117c7f
dknight <dmitri@dmitri.me>
2010-11-24 13:08:15 +0200

    first commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,30 @@
+require "rubygems"
+require "spec"
+require "spec/interop/test"
+require "rack/test"
+require "rspec_hpricot_matchers"
+require "sinatra"
+
+Test::Unit::TestCase.send :include, Rack::Test::Methods
+
+Spec::Runner.configure do |config|
+  config.include(RspecHpricotMatchers)
+end
+
+set :views => File.join(File.dirname(__FILE__), "..", "views"),
+    :public => File.join(File.dirname(__FILE__), "..", "public")
+
+set :environment, :test
+set :reload_templates, true
+
+require File.join(File.dirname(__FILE__), "..", "app")
+
+module RequestSpecHelper
+  def app
+    Nesta::App
+  end
+  
+  def body
+    last_response.body
+  end
+end
